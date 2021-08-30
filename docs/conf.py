986016e75e467f33d9597e8158dad57f2f1cf37d
7d <61975820+iiiii7d@users.noreply.github.com>
2021-08-30 11:19:40 +0000

    add sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -74,9 +74,11 @@ html_theme_options = {
 
     'base_url': 'https://tegen.readthedocs.io/en/{}/'.format(branch)
 }
+html_sidebars = {
+    "**": ["globaltoc.html"]
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
-html_static_path = []
+html_static_path = ['_static']
